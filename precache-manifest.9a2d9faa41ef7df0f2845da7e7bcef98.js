89ef4a1d6e51ac0c1c5731fda99a23118415758d
MCasante <math.casante@gmail.com>
2020-08-24 00:03:17 -0300

    Updates

---
+++ precache-manifest.9a2d9faa41ef7df0f2845da7e7bcef98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3063941a2926c8d7c5a458e907866620",
+    "url": "/wordcrawler-ui/index.html"
+  },
+  {
+    "revision": "94adfacc348c8093d02b",
+    "url": "/wordcrawler-ui/static/css/main.d6224036.chunk.css"
+  },
+  {
+    "revision": "4e1df6e04ea391598aee",
+    "url": "/wordcrawler-ui/static/js/2.47622e62.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wordcrawler-ui/static/js/2.47622e62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94adfacc348c8093d02b",
+    "url": "/wordcrawler-ui/static/js/main.c17183fc.chunk.js"
+  },
+  {
+    "revision": "ccf94a5761202ae4ac1f",
+    "url": "/wordcrawler-ui/static/js/runtime-main.d739cdf9.js"
+  }
+]);
